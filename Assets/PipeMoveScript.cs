ba3c9a7433a336c119260090e9b2ba4f2c2a1483
Ferris <ferris.evans@outlook.com>
2024-07-02 22:59:40 +0800

    pipe

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    // If we give it a value here, It will fill this as the default value in Unity.
+    public float moveSpeed = 5.0F;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * (Time.deltaTime * moveSpeed);
+    }
+}
